480a5a311257a839bc478e2ef14f79169826b0eb
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-30 13:46:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const sendMsg = require("./webhook.js");
 module.exports = {
 modlog(client, reason, title, author, mention, Discord){
-
+ 
 },
-
+ 
 log(client, channel, Discord){
 let color = "#0099FF";
 
