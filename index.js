62319c99999b7fd73790bb921934210d8c2a5462
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-11-11 16:01:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ spamText(client, channel, modch, Discord){
 client.on("message", msg => {
 
 if(msg.channel.type === "dm") return;
-if(!msg.channel.guild.id === message.guild.id) return;
 
 if(channel.includes(msg.channel.id)){
 
@@ -40,6 +39,25 @@ if(msg.content === st.get(msg.channel.id)){
  msg.delete().catch( );
   msg.channel.send({ embed: color: 0xFF0000, title: "spam some Text", description: `${msg.author.id} please no repeat some text` }}).then(md => md.delete({ timeout: 3000 }).catch( );
  st.delete(msg.channel.id+":"+msg.author.id);
+
+  if(modch){
+
+if(!client.channels.cache.get(modch).guild.id === msg.guild.id) return;
+if(client.channels.cache.get(modch).id === msg.channel.id) return;
+
+ sendMsg({
+channel: client.channels.cache.get(modch),
+discord: Discord,
+bot: client,
+message: new Discord.MessageEmbed()
+.setColor("#FF0000")
+.setTitle("Spam some Messages")
+.setThumbnail(message.author.displayAvatarURL())
+.setDescription(`**Message Channel**- ${msg.channel.name}\n**Message User**- <@${msg.author.id}>(@${msg.author.tag})`)
+.setTimestamp();
+}); 
+ }
+
 }
 
   }else {
@@ -58,6 +76,8 @@ client.on("messageDelete", message => {
 
 if(channel.type === "dm") return;
 if(!channel.guild.id === message.guild.id) return;
+if(channel.id === message.channel.id) return;
+
 sendMsg({
 channel: channel,
 discord: Discord,
